d7b1ff28d17f8b724d7dedc4c2e1b15c85f5c53b
wojtaskuba <73549865+wojtaskuba@users.noreply.github.com>
2021-01-27 10:32:05 +0100

    Add files via upload

---
+++ Jakub_Wojtas_zad10.py
@@ -0,0 +1,18 @@
+import re
+bridge_of_death = '''
+-Jaki jest twój ulubiony kolor?
+-Niebieski!
+-Dobrze. Idź.
+...
+-Stój. Jakie jest twe imię?
+-Sir Galahad z Camelotu.
+-Jaki jest twój cel?
+-Odnaleźć Graala.
+-Jaki jest twój ulubiony kolor?
+-Niebieski... nie... żóóóółtyyyy!
+'''
+
+
+x = re.compile(r'(\b\w{4,}\b)')
+words = set(x.findall(bridge_of_death))
+print(words)
